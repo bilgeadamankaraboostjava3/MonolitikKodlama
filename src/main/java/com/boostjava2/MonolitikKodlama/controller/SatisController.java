70c8a8d2f263fb2985fefe095970c3390dcff13a
bilgeadamankaraboostjava3 <107560948+bilgeadamankaraboostjava3@users.noreply.github.com>
2022-09-09 14:10:48 +0300

    Satis controller eklendi.

--- src/main/java/com/boostjava2/MonolitikKodlama/controller/SatisController.java
+++ src/main/java/com/boostjava2/MonolitikKodlama/controller/SatisController.java
@@ -1,10 +1,23 @@
 package com.boostjava2.MonolitikKodlama.controller;
 
+import com.boostjava2.MonolitikKodlama.services.SatisService;
+import com.boostjava2.MonolitikKodlama.utility.Datas;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/satis")
+@RequiredArgsConstructor
 public class SatisController {
 
+    private final SatisService satisService;
+
+    @GetMapping("/savealldemo")
+    public ResponseEntity<String> saveAllDemo(){
+        satisService.saveAll(new Datas().getSatisList());
+        return ResponseEntity.ok("Kayıtlar Eklendi.");
+    }
 }
