3fbbca38dafd1d8428d43249152d167696aac75d
bilgeadamankaraboostjava3 <107560948+bilgeadamankaraboostjava3@users.noreply.github.com>
2022-09-12 16:51:36 +0300

    Satış için HTML tasarımı eklendi

--- src/main/java/com/boostjava2/MonolitikKodlama/controllermvc/MusteriMvcController.java
+++ src/main/java/com/boostjava2/MonolitikKodlama/controllermvc/MusteriMvcController.java
@@ -5,18 +5,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @RequestMapping("/WEB/musteri")
 public class MusteriMvcController {
     /**
-     *
+     * http://localhost:9090/WEB/musteri/index
      * @return
      */
     @GetMapping("/index")
     public ModelAndView index(){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("musteri");
+        String manset = "Thymeleaf ile MVC";
+        List<String> isimlistesi =
+                Arrays.asList("Ahmet","Mehmet","Ali","Veli","Ayşe","Fatma","Zeynep");
+        modelAndView.addObject("isimler",isimlistesi);
+        modelAndView
+                .addObject("manset",manset);
         return modelAndView;
+
     }
 
 }
