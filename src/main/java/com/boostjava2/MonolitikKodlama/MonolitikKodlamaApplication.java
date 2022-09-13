cb75b1ac549e3c47711f49cbeafeaf3851d3cd1b
bilgeadamankaraboostjava3 <107560948+bilgeadamankaraboostjava3@users.noreply.github.com>
2022-09-13 14:53:40 +0300

    Başlangıçta tüm dataların yüklenmesi için StartPage e constructor ekledim ve içine dataları yüklemesin için kodlama yaptım.

--- src/main/java/com/boostjava2/MonolitikKodlama/MonolitikKodlamaApplication.java
+++ src/main/java/com/boostjava2/MonolitikKodlama/MonolitikKodlamaApplication.java
@@ -1,11 +1,28 @@
 package com.boostjava2.MonolitikKodlama;
 
+import com.boostjava2.MonolitikKodlama.services.MusteriService;
+import com.boostjava2.MonolitikKodlama.services.SatisService;
+import com.boostjava2.MonolitikKodlama.services.UrunService;
+import com.boostjava2.MonolitikKodlama.utility.Datas;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
+@RequiredArgsConstructor
 public class MonolitikKodlamaApplication {
 
+	private final SatisService satisService;
+	private final UrunService urunService;
+	private final MusteriService musteriService;
+	@PostConstruct
+	private void implData(){
+		musteriService.saveAll(new Datas().getMusteriList());
+		urunService.saveAll(new Datas().getUrunList());
+		satisService.saveAll(new Datas().getSatisList());
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MonolitikKodlamaApplication.class, args);
 	}
